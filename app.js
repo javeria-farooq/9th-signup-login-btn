63b2ac93297bc8279bafdd00e6625b7261b9c906
javeria-farooq <gettingeducation6@gmail.com>
2024-01-17 09:58:40 +0500

    9th JS Assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+// var email = prompt("Enter your email or user name:")
+// var password = prompt("Enter your password:")
+
+// if(email == password) {
+//     alert("Login successfully")
+// }
+// else{
+//     alert("Invalid username or password")
+// }
+
+// var email = prompt("email")
+// var password = prompt("password")
+
+
+// if(email == "afnan" && password == 123) {
+//     alert("Login successful")
+// }
+// else{
+//     alert("invalid")
+// }
